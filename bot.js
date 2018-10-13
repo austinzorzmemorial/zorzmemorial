cecc3def276a20d6860674f4ec1972569003a9e9
austinzorzmemorial <44125091+austinzorzmemorial@users.noreply.github.com>
2018-10-13 23:49:00 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
